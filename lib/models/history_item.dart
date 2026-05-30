a6215f8c935c06eb9f110e5f99d0c8b78e7321bc
Rocky3697 <rockyloveaadha114@gmail.com>
2026-05-30 11:34:56 +0530

    Update history_item.dart

--- lib/models/history_item.dart
+++ lib/models/history_item.dart
@@ -1,1 +1,19 @@
+class HistoryItem {
+  final String equation;
+  final String result;
+  final DateTime dateTime;
 
+  HistoryItem({required this.equation, required this.result, required this.dateTime});
+
+  Map<String, dynamic> toMap() => {
+    'equation': equation,
+    'result': result,
+    'dateTime': dateTime.toIso8601String(),
+  };
+
+  factory HistoryItem.fromMap(Map<String, dynamic> map) => HistoryItem(
+    equation: map['equation'],
+    result: map['result'],
+    dateTime: DateTime.parse(map['dateTime']),
+  );
+}
