1de5403a3cac75b3e7e4c9aadc514ada27a0dd4d
Rocky3697 <rockyloveaadha114@gmail.com>
2026-05-30 11:37:35 +0530

    Update theme_provider.dart

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = true;
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() { _loadTheme(); }
+
+  void toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', _isDarkMode);
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool('isDarkMode') ?? true;
+    notifyListeners();
+  }
+
+  ThemeData get currentTheme => ThemeData(
+    useMaterial3: true,
+    brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+    colorSchemeSeed: Colors.deepPurple,
+  );
+}
