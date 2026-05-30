264600b0e80b942ea729ea8b4733c0c729a3a2ac
Rocky3697 <rockyloveaadha114@gmail.com>
2026-05-30 13:39:13 +0530

    Update calculator_service.dart

--- lib/services/calculator_service.dart
+++ lib/services/calculator_service.dart
@@ -1,1 +1,15 @@
+import 'package:math_expressions/math_expressions.dart';
 
+class CalculatorService {
+  String evaluate(String expression) {
+    try {
+      String finalExpr = expression.replaceAll('×', '*').replaceAll('÷', '/');
+      Parser p = Parser();
+      Expression exp = p.parse(finalExpr);
+      double eval = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return eval.toStringAsFixed(eval == eval.toInt() ? 0 : 2);
+    } catch (e) {
+      return "Error";
+    }
+  }
+}
