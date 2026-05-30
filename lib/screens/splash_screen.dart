8e06c7aa0fb44872dc639cff58dc426a9ebd81f7
Rocky3697 <rockyloveaadha114@gmail.com>
2026-05-30 13:52:30 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(child: Icon(Icons.calculate, size: 100, color: Colors.white)),
+    );
+  }
+}
